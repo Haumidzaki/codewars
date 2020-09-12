fe68f17db9ea7255e0f9b5a14315208ff7b5d3a0
Haumidzaki <48284838+Haumidzaki@users.noreply.github.com>
2020-09-12 12:13:37 +0300

    Update CamelCaseMethod.java

--- CamelCaseMethod.java
+++ CamelCaseMethod.java
@@ -1,3 +1,14 @@
+/*
+Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. 
+All words must have their first letter capitalized without spaces.
+
+For instance:
+
+camelCase("hello case"); // => "HelloCase"
+camelCase("camel case word"); // => "CamelCaseWord"
+Don't forget to rate this kata! Thanks :)
+*/
+
 public class CamelCaseMethod {
 
     public static String camelCase(String str) {
